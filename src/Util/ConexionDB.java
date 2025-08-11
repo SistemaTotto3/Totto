0472b33066007cc743e02a43e4feabaebd57b6ad
AlexanderOcon <brandon.hermida.ocon@gmail.com>
2025-08-11 12:07:49 -0600

    Agregar XD

--- src/Util/ConexionDB.java
+++ src/Util/ConexionDB.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Util;
 
 import java.sql.Connection;
@@ -16,7 +12,7 @@ public class ConexionDB {
 
     private static final String URL = "jdbc:mysql://localhost:3306/Sistematotto?useSSL=false";
     private static final String USER = "root";
-    private static final String PASSWORD = "monge123";
+    private static final String PASSWORD = "Alexander12";
  
     public static Connection getConnection() throws SQLException {
         try {
